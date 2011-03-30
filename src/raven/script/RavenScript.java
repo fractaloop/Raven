b1be31bc5a9a397927230ff7d75eb3ae66f03475
Logan Lowell <fractaloop@thefrontside.net>
2011-03-30 13:40:55 -0500

    RavenScript stub.

---
+++ src/raven/script/RavenScript.java
@@ -0,0 +1,19 @@
+package raven.script;
+
+public class RavenScript {
+
+	public static String getString(String name) {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	public static int getInt(String name) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static double getDouble(String name) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
