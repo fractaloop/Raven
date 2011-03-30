9ccde4f07f2c0caace079a50debfc362c31d47e2
Logan Lowell <fractaloop@thefrontside.net>
2011-03-30 17:00:01 -0500

    Needed some UserOptions.

---
+++ src/raven/game/RavenUserOptions.java
@@ -0,0 +1,30 @@
+package raven.game;
+
+public class RavenUserOptions {
+	  public static boolean showGraph;
+
+	  public static boolean showNodeIndices;
+
+	  public static boolean showPathOfSelectedBot;
+
+	  public static boolean showTargetOfSelectedBot;
+
+	  public static boolean showOpponentsSensedBySelectedBot;
+
+	  public static boolean onlyShowBotsInTargetsFOV;
+
+	  public static boolean showGoalsOfSelectedBot;
+
+	  public static boolean showGoalAppraisals;
+
+	  public static boolean showWeaponAppraisals;
+
+	  public static boolean smoothPathsQuick;
+	  public static boolean smoothPathsPrecise;
+
+	  public static boolean showBotIDs;
+
+	  public static boolean showBotHealth;
+
+	  public static boolean showScore;
+}
