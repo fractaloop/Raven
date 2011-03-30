6f6672d979ed166f408da4ec5fa1f0fbf3826c9e
Logan Lowell <fractaloop@thefrontside.net>
2011-03-30 16:59:51 -0500

    Added some messaging stuf.

---
+++ src/raven/game/messaging/Dispatcher.java
@@ -0,0 +1,92 @@
+package raven.game.messaging;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+import raven.game.BaseGameEntity;
+import raven.game.EntityManager;
+
+public class Dispatcher {
+	// Singleton Dispatcher, just like the original.
+	private static class DispatcherHolder {
+		public static final Dispatcher INSTANCE = new Dispatcher();
+	}
+
+	public static Dispatcher getInstance() {
+		return DispatcherHolder.INSTANCE;
+	}
+	
+	/** 
+	 * a TreeSet is used as the container for the delayed message because of
+	 * the benefit of automatic sorting and avoidance of duplicates. Messages
+	 * are sorted by their dispatch time. */
+	private TreeSet<Telegram> priorityQueue;
+	
+	private static void discharge(BaseGameEntity receiver, Telegram msg) {
+		if (!receiver.handleMessage(msg)) {
+			// Receiver couldn't handle message
+		}
+	}
+	
+	private Dispatcher() { }
+
+	public static final double SEND_MSG_IMMEDIATELY = 0.0;
+	public static final int    NO_ADDITIONAL_INFO   = 0;
+	public static final int    SENDER_ID_IRRELEVANT = -1;
+	
+	/**
+	 * send a message to another agent. Receiving agent is referenced by ID.
+	 * @param delay Delay in seconds until the message should be sent
+	 * @param senderID the ID of the sender
+	 * @param receiverID the ID of the sender 
+	 * @param msg the type of the message to send
+	 * @param extraInfo optional object to attach to the message
+	 */
+	public static void dispatchMsg(double delay,
+							int senderID,
+							int receiverID,
+							RavenMessage msg,
+							Object extraInfo) {
+		
+		// get the receiver
+		BaseGameEntity receiver = EntityManager.getEntityFromID(receiverID);
+		
+		// make sure the receiver is valid
+		if (receiver == null) {
+			System.err.println("Warning! No receiver with ID of " + receiverID + " found.");
+			return;
+		}
+		
+		// create the telegram
+		Telegram telegram = new Telegram(0, senderID, receiverID, msg, extraInfo);
+		
+		// if there is no delay, route telegram immediately
+		if (delay <= 0.0) {
+			discharge(receiver, telegram);
+		}
+		// else add the telegram to be dispatched
+		else {
+			telegram.dispatchTime = System.nanoTime() + (long)(delay * 1e9);
+			
+			getInstance().priorityQueue.add(telegram);
+		}
+	}
+	
+	/**
+	 * send out any delayed messages. This method is called each time through
+	 * the main game loop.
+	 */
+	public static void dispatchDelayedMessages() {
+		HashSet<Telegram> toRemove = new HashSet<Telegram>();
+		for (Telegram telegram : getInstance().priorityQueue) {
+			if (telegram.dispatchTime < System.nanoTime()) {
+				toRemove.add(telegram);
+				BaseGameEntity receiver = EntityManager.getEntityFromID(telegram.receiverID);
+				discharge(receiver, telegram);
+			}
+		}
+		getInstance().priorityQueue.removeAll(toRemove);
+	}
+}
+
+
--- src/raven/game/messaging/Telegram.java
+++ src/raven/game/messaging/Telegram.java
@@ -1,5 +1,29 @@
 package raven.game.messaging;
 
-public class Telegram {
-
+public class Telegram implements Comparable<Telegram> {
+	public long dispatchTime;
+	public int senderID;
+	public int receiverID;
+	public RavenMessage msg;
+	public Object extraInfo;
+	
+	public Telegram() {
+		dispatchTime = -1;
+		senderID = -1;
+		receiverID = -1;
+		msg = RavenMessage.MSG_BLANK;
+		
+	}
+	public Telegram(long dispatchTime, int senderID, int receiverID, RavenMessage msg, Object extraInfo) {
+		this.dispatchTime = dispatchTime;
+		this.senderID = senderID;
+		this.receiverID = receiverID;
+		this.msg = msg;
+		this.extraInfo = extraInfo;
+	}
+	
+	@Override
+	public int compareTo(Telegram other) {
+		return (int)(this.dispatchTime - other.dispatchTime);
+	}
 }
