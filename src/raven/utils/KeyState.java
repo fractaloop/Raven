5c0fc630a4d3047cadeca7807b25cafba95f05df
Logan Lowell <fractaloop@thefrontside.net>
2011-03-31 11:25:59 -0500

    Ooops...This was part of the new Key management stuff for the Raven class

---
+++ src/raven/utils/KeyState.java
@@ -0,0 +1,32 @@
+package raven.utils;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.TreeSet;
+
+public class KeyState implements KeyListener {
+	private TreeSet<Integer> keys;
+	
+	public KeyState() {
+		keys = new TreeSet<Integer>();
+	}
+	
+	public boolean isKeyPressed(int key) {
+		return keys.contains(key);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		keys.add(event.getKeyCode());
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		keys.remove(event.getKeyCode());
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {}
+}
