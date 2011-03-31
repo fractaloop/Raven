884cbb0b743d9ee09484161a08b261354cd35a51
Logan Lowell <fractaloop@thefrontside.net>
2011-03-31 14:30:47 -0500

    Some more stubs

---
+++ src/raven/math/Transformations.java
@@ -0,0 +1,13 @@
+package raven.math;
+
+import java.util.List;
+
+public class Transformations {
+
+	public static List<Vector2D> WorldTransform(List<Vector2D> vecBotVB,
+			Vector2D pos, Vector2D facing, Vector2D perp, Vector2D scale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
