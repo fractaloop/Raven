a462718064871c154ee0cd9c648bf0dfacbf8029
Logan Lowell <fractaloop@thefrontside.net>
2011-03-31 11:41:52 -0500

    Added RavenBot variables

--- src/raven/game/RavenBot.java
+++ src/raven/game/RavenBot.java
@@ -1,15 +1,90 @@
 package raven.game;
 
+import java.util.ArrayList;
+
 import raven.math.Vector2D;
 
 public class RavenBot extends MovingEntity {
+	private enum Status {
+		ALIVE,
+		DEAD,
+		SPAWNING
+	}
+	
+	/** alive, dead or spawning? */
+	private Status status;
+	
+	/** a pointer to the world data */
+	private RavenGame world;
+	
+	/** this object handles the arbitration and processing of high level goals
+	 */
+	private GoalThink brain;
+	
+	/** this is a class that acts as the bots sensory memory. Whenever this
+	 * bot sees or hears an opponent, a record of the event is updated in the
+	 * memory. */
+	private RavenSensoryMemory sensoryMem;
+	
+	/** the bot uses this object to steer */
+	private RavenSteering steering;
+	
+	/** the bot uses this object to plan paths */
+	private RavenPathPlanner pathPlanner;
+	
+	/** this is responsible for choosing the bot's current target */
+	private RavenTargetingSystem targSys;
+	
+	/** this handles all the weapons. and has methods for aiming, selecting
+	 * and shooting them */
+	private RavenWeaponSystem weaponSys;
+	
+	/** A regulator object limits the update frequency of a specific AI
+	 * component */
+	private Regulator weaponSelectionRegulator;
+	private Regulator goalArbitrationRegulator;
+	private Regulator targetSelectionRegulator;
+	private Regulator triggerTestRegulator;
+	private Regulator visionUpdateRegulator;
+	
+	/** the bot's health. Every time the bot is shot this value is decreased.
+	 * If it reaches zero then the bot dies (and respawns) */
+	private int health;
+	
+	/** the bot's maximum health value. It starts its life with health at this
+	 * value */
+	private int maxHealth;
+	
+	/** each time this bot kills another this value is incremented */
+	private int score;
+	
+	/** the direction the bot is facing (and therefore the direction of aim).
+	 * Note that this may not be the same as the bot's heading, which always
+	 * points in the direction of the bot's movement */
+	private Vector2D facing;
+	
+	/** a bot only perceives other bots within this field of view */
+	private double fieldOfView;
+	
+	/** to show that a player has been hit it is surrounded by a thick red
+	 * circle for a fraction of a second. This variable represents
+	 * the number of update-steps the circle gets drawn */
+	private int numSecondsHitPersistant;
+	
+	/** set to true when the bot is hit, and remains true until
+	 * numSecondsHitPersistant becomes zero. (used by the render method to
+	 * draw a thick red circle around a bot to indicate it's been hit) */
+	private boolean hit;
 
-	private static double botScale = .8;
-	private static double botMaxSpeed = 1;
-	private static double botMass = 1;
-	private static double botMaxForce = 1.0;
-	private static double botMaxHeadTurnRate = .2;
-	private static int botMaxHealth = 100;
+	/** set to true when a human player takes over control of the bot */
+	private boolean possessed;
+	
+	/** a vertex buffer containing the bot's geometry */
+	private ArrayList<Vector2D> vecBotVB;
+	
+	/** the buffer for the transformed vertices */
+	private ArrayList<Vector2D> vecBotVBTrans;
+	
 	
 	
 	public RavenBot(RavenGame world, Vector2D position) {
