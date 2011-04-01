b1f5e5e1eb5679e6a6b887b5f1970c149dadb38c
Logan Lowell <fractaloop@thefrontside.net>
2011-03-31 19:52:35 -0500

    Finished RavenTargetingSystem

--- src/raven/game/RavenTargetingSystem.java
+++ src/raven/game/RavenTargetingSystem.java
@@ -1,43 +1,67 @@
 package raven.game;
 
+import java.util.List;
+
+import raven.math.Vector2D;
+
 public class RavenTargetingSystem {
 	
-	private RavenBot target;
+	private RavenBot owner;
+	
+	private RavenBot currentTarget;
 
-	public RavenTargetingSystem(RavenBot ravenBot) {
-		// TODO Auto-generated constructor stub
+	public RavenTargetingSystem(RavenBot owner) {
+		this.owner = owner;
 	}
 
 	public void update() {
-		// TODO Auto-generated method stub
+		double closestDistSoFar = Double.MAX_VALUE;
+		currentTarget = null;
 		
+		// grab a list of all the opponents the owner can sense
+		List<RavenBot> sensedBots = owner.getSensoryMem().getListOfRecentlySensedOpponents();
+		
+		for (RavenBot opponent : sensedBots) {
+			if (opponent.isAlive() && !opponent.equals(owner)) {
+				double dist = opponent.pos().distanceSq(owner.pos());
+				
+				if (dist < closestDistSoFar) {
+					closestDistSoFar = dist;
+					currentTarget = opponent;
+				}
+			}
+		}
 	}
-
-	public void clearTarget() {
-		target = null;
+	
+	public boolean isTargetPresent() {
+		return currentTarget != null;
+	}
+	
+	public boolean isTargetWithinFOV() {
+		return owner.getSensoryMem().isOpponentWithinFOV(currentTarget);
+	}
+	
+	public boolean isTargetShootable() {
+		return owner.getSensoryMem().isOpponentShootable(currentTarget);
+	}
+	
+	public Vector2D getLastRecordedPosition() {
+		return owner.getSensoryMem().getLastRecordedPositionOfOpponent(currentTarget);
+	}
+	
+	public double getTimeTargetHasBeenVisible() {
+		return owner.getSensoryMem().getTimeOpponentHasBeenVisible(currentTarget);
+	}
+	
+	public double getTimeTargetHasBeenOutOfView() {
+		return owner.getSensoryMem().getTimeOpponentHasBeenOutOfView(currentTarget);
 	}
 
 	public RavenBot getTarget() {
-		return target;
+		return currentTarget;
 	}
-
-	public RavenTargetingSystem(RavenBot ravenBot) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public void update() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	public void clearTarget() {
-		// TODO Auto-generated method stub
-		
+		currentTarget = null;
 	}
-
-	public RavenBot getTarget() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
