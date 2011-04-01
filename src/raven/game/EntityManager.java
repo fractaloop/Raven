f928efde9f6bae21c219e83e777c01d39fb5ec86
Logan Lowell <fractaloop@thefrontside.net>
2011-03-31 19:59:19 -0500

    Finished the EntityManager

--- src/raven/game/EntityManager.java
+++ src/raven/game/EntityManager.java
@@ -1,20 +1,37 @@
 package raven.game;
 
-public class EntityManager {
+import java.util.HashMap;
+import java.util.Map;
 
-	public static void reset() {
-		// TODO Auto-generated method stub
-		
+public class EntityManager {
+	private static class EntityManagerHolder {
+		public static final EntityManager INSTANCE = new EntityManager();
+	}
+
+	public static EntityManager getInstance() {
+		return EntityManagerHolder.INSTANCE;
+	}
+	
+	private Map<Integer, BaseGameEntity> entityMap = new HashMap<Integer, BaseGameEntity>();
+	
+	private EntityManager() {}
+	
+	public static void registerEntity(RavenBot bot) {
+		getInstance().entityMap.put(bot.ID(), bot);
 	}
 
 	public static BaseGameEntity getEntityFromID(int receiverID) {
-		// TODO Auto-generated method stub
-		return null;
+		return getInstance().entityMap.get(receiverID);
+	}
+	
+	public static void removeEntity(BaseGameEntity entity) {
+		getInstance().entityMap.remove(entity);
 	}
 
-	public static void registerEntity(RavenBot bot) {
-		// TODO Auto-generated method stub
-		
+	public static void reset() {
+		getInstance().entityMap.clear();
 	}
+
+
 
 }
