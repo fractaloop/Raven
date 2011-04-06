28c6fb2a851789f7616851a00de993f5a398204e
Logan Lowell <fractaloop@thefrontside.net>
2011-04-06 13:24:28 -0500

    Generic Pair class that seems like it might come in handy for the Graph crap

---
+++ src/raven/util/Pair.java
@@ -0,0 +1,22 @@
+package raven.util;
+
+public class Pair<T1, T2> {
+	public T1 first;
+	public T2 second;
+	
+	public Pair(T1 first, T2 second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj instanceof Pair) {
+			Pair other = (Pair)obj;
+			if (first.equals(other.first) && second.equals(other.second)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
