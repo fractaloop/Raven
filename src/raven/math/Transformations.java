1bf934148bcda7d524a2c98bc990432e01c20b3c
Logan Lowell <fractaloop@thefrontside.net>
2011-04-06 13:21:09 -0500

    Adding Transform

--- src/raven/math/Transformations.java
+++ src/raven/math/Transformations.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 public class Transformations {
 
+	public static void Vec2DRotateAroundOrigin(Vector2D v, double angle) {
+		C2DMatrix mat = new C2DMatrix();
+		
+		mat.rotate(angle);
+		
+		mat.transformVector2Ds(v);
+	}
+	
 	public static List<Vector2D> WorldTransform(List<Vector2D> vecBotVB,
 			Vector2D pos, Vector2D facing, Vector2D perp, Vector2D scale) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
