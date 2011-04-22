c1b553b8bddadc02607a84b34374c630eadefd33
Logan Lowell <fractaloop@thefrontside.net>
2011-04-22 00:03:30 -0500

    Make the menubar look better on Macs

--- RavenEdit/src/raven/edit/RavenEdit.java
+++ RavenEdit/src/raven/edit/RavenEdit.java
@@ -4,6 +4,8 @@ import raven.edit.editor.EditorController;
 
 public class RavenEdit {
 	public static void main(String[] args) {
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
+		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new EditorController();
