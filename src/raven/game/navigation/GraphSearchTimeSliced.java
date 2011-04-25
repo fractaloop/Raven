30ed4a295f8dc417c00a011dc9209b300a288748
Logan Lowell <fractaloop@thefrontside.net>
2011-04-25 11:50:38 -0500

    Indentation correction.

--- src/raven/game/navigation/GraphSearchTimeSliced.java
+++ src/raven/game/navigation/GraphSearchTimeSliced.java
@@ -3,30 +3,26 @@ package raven.game.navigation;
 import java.util.List;
 import java.util.Vector;
 
-import raven.math.graph.GraphEdge;
-import raven.math.graph.GraphEdgeFactory;
-import raven.math.graph.GraphNode;
 import raven.math.graph.SparseGraph;
-import raven.utils.StreamUtils;
-import raven.math.graph.GraphSearchDijkstra;
 
 public class GraphSearchTimeSliced<NodeType extends NavGraphNode<T>,EdgeType extends NavGraphEdge, T>  {
 
 	public enum SearchType{AStar, Dijkstra};
-	  
+
 	private Vector<Double> costToThisNode; 
 
 	private  Vector<EdgeType>  shortestPathTree;
-	 private Vector<EdgeType>  searchFrontier;
+	private Vector<EdgeType>  searchFrontier;
 
-	 private int source;
-	 private int target;
-	 private SparseGraph<NodeType, EdgeType> graph=new SparseGraph<NodeType, EdgeType>();
+	private int source;
+	private int target;
+	private SparseGraph<NodeType, EdgeType> graph=new SparseGraph<NodeType, EdgeType>();
 	public List<PathEdge> path;
 	private SearchType searchType;
 	public GraphSearchTimeSliced(){
-		
+
 	}
+
 	public SearchType GetType(){
 		return searchType;
 	}
@@ -38,21 +34,21 @@ public class GraphSearchTimeSliced<NodeType extends NavGraphNode<T>,EdgeType ext
 	public List<PathEdge> getPathAsPathEdges(){
 		//List<PathEdge> path= new List<PathEdge>();
 
-		  //just return an empty path if no target or no path found
-		  if (target < 0)  return path;    
+		//just return an empty path if no target or no path found
+		if (target < 0)  return path;    
 
-		  int nd = target;
-		    
-		  while ((nd != source) && (shortestPathTree.get(nd) != null))
-		  {
-		    path.add(new PathEdge(graph.getNode(shortestPathTree.get(nd).from()).pos(),
-		    		graph.getNode(shortestPathTree.get(nd).to()).pos(),
-		                             shortestPathTree.get(nd).flags(),shortestPathTree.get(nd).IDOfIntersectingEntity()));
+		int nd = target;
 
-		    nd = shortestPathTree.get(nd).from();
-		  }
+		while ((nd != source) && (shortestPathTree.get(nd) != null))
+		{
+			path.add(new PathEdge(graph.getNode(shortestPathTree.get(nd).from()).pos(),
+					graph.getNode(shortestPathTree.get(nd).to()).pos(),
+					shortestPathTree.get(nd).flags(),shortestPathTree.get(nd).IDOfIntersectingEntity()));
 
-		  return path;
+			nd = shortestPathTree.get(nd).from();
 		}
-	
+
+		return path;
+	}
+
 }
