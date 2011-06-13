feb5b171e55284073f1851efec01bd78f27a3200
Chester Husk III <chusk3@gmail.com>
2011-06-13 15:25:39 -0500

    Cleaning up WeaponSystemTests because there was a lot of duplicate setup.

--- src/raven/game/test/WeaponSystemTest.java
+++ src/raven/game/test/WeaponSystemTest.java
@@ -7,6 +7,7 @@ import junit.framework.Assert;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.junit.Before;
 import org.junit.Test;
 
 import raven.game.RavenObject;
@@ -25,14 +26,23 @@ import raven.math.Vector2D;
  */
 public class WeaponSystemTest {
 
+	Mockery mocker;
+	IRavenBot bot;
+	IRavenBot target;
+	IRavenTargetingSystem targeting;
+	Vector2D botPos;
+	
+	@Before
+	public void SetUp(){
+		mocker = new Mockery();
+		bot = mocker.mock(IRavenBot.class, "sourceBot");
+		target = mocker.mock(IRavenBot.class, "targetBot");
+		targeting = mocker.mock(IRavenTargetingSystem.class);
+		botPos = new Vector2D(5, 5);
+	}
+	
 	@Test
 	public void Weapon_Chosen_With_Close_Enemy_Is_Shotgun(){
-		
-		Mockery mocker = new Mockery();
-		final IRavenBot bot = mocker.mock(IRavenBot.class, "sourceBot");
-		final IRavenBot target = mocker.mock(IRavenBot.class, "targetBot");
-		final IRavenTargetingSystem targeting = mocker.mock(IRavenTargetingSystem.class);
-		final Vector2D botPos = new Vector2D(5, 5);
 		
 		final Vector2D targetPos = new Vector2D(10, 10);
 		
@@ -55,12 +65,7 @@ public class WeaponSystemTest {
 	
 	@Test
 	public void Weapon_Chosen_With_Far_Enemy_Is_Rail_Gun(){
-		Mockery mocker = new Mockery();
-		final IRavenBot bot = mocker.mock(IRavenBot.class, "sourceBot");
-		final IRavenBot target = mocker.mock(IRavenBot.class, "targetBot");
-		final IRavenTargetingSystem targeting = mocker.mock(IRavenTargetingSystem.class);
-		final Vector2D botPos = new Vector2D(5, 5);
-		
+			
 		final Vector2D targetPos = new Vector2D(150, 150);
 		
 		mocker.checking(new Expectations(){{
@@ -82,12 +87,6 @@ public class WeaponSystemTest {
 	
 	@Test
 	public void Weapon_Chosen_When_Medium_Range_Is_Rocket(){
-		Mockery mocker = new Mockery();
-		final IRavenBot bot = mocker.mock(IRavenBot.class, "sourceBot");
-		final IRavenBot target = mocker.mock(IRavenBot.class, "targetBot");
-		final IRavenTargetingSystem targeting = mocker.mock(IRavenTargetingSystem.class);
-		final Vector2D botPos = new Vector2D(5, 5);
-		
 		final Vector2D targetPos = new Vector2D(100, 100);
 		
 		mocker.checking(new Expectations(){{
@@ -109,11 +108,6 @@ public class WeaponSystemTest {
 	
 	@Test
 	public void Weapon_Chosen_When_Close_And_Empty_Railgun_Is_Blaster(){
-		Mockery mocker = new Mockery();
-		final IRavenBot bot = mocker.mock(IRavenBot.class, "sourceBot");
-		final IRavenBot target = mocker.mock(IRavenBot.class, "targetBot");
-		final IRavenTargetingSystem targeting = mocker.mock(IRavenTargetingSystem.class);
-		final Vector2D botPos = new Vector2D(5, 5);
 		
 		final Vector2D targetPos = new Vector2D(10, 10);
 		
